56611e76643328dc44b003ff1ec91c0fc2564dc9
Igor Khryukin <igor.hryukin@yandex.ru>
2022-03-22 23:40:30 +0700

    Попытки в статистический анализ

--- src/GitLearning.java
+++ src/GitLearning.java
@@ -1,8 +1,10 @@
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class GitLearning {
@@ -18,22 +20,27 @@ public class GitLearning {
         int key = 3;
         Path path = Path.of("Texts\\test.txt");
         Path path2 = Path.of("Texts\\encode.txt");
+        Path path3 = Path.of("Texts\\encrypted.txt");
+        Path path4 = Path.of("Texts\\whiteguard.txt");
+        Path path5 = Path.of("Texts\\encodeWithAnalize.txt");
+
+
         String t = "test.txt";
 
 
-        encodeFunction(path, key);
-        decodeFunction(path2, key);
+        //encodeFunction(path, key);
+        //decodeFunction(path2, key);
 
-        //Map<Character, Integer> testMap = charsCounter(path);
+        //Map<Character, Integer> testMap = charsCounter(Path.of("Texts\\encrypted.txt"));
         //System.out.println(testMap);
         //Map<Character, Integer> encodeMap = charsCounter(path2);
         //System.out.println(encodeMap);
-        LocalTime localTime1 = LocalTime.now();
-        System.out.println(localTime1);
-        //bruteForceDecoder(path2);
-        LocalTime localTime2 = LocalTime.now();
-        System.out.println(localTime2);
-        System.out.println(ALPHABET.length);
+        //LocalTime localTime1 = LocalTime.now();
+        //System.out.println(localTime1);
+        bruteForceDecoder(path3);
+        //LocalTime localTime2 = LocalTime.now();
+        //System.out.println(localTime2);
+
 
         /*Map<Character, Integer> newMap = charsCounter(path);
 
@@ -42,17 +49,16 @@ public class GitLearning {
             System.out.println(ch + " - " + newMap.get(ch));
         }*/
 
+        Map<Character, Integer> enc = charsCounter(path3);
+        Map<Character, Integer> white = charsCounter(path4);
 
+        System.out.println(enc);
+        System.out.println(white);
 
-        /*try(BufferedReader bufferedReader = new BufferedReader(new FileReader(String.valueOf(path)))){
-            String str = bufferedReader.readLine();
-            if(str.contains(" и ") || str.contains(" в ") || str.contains(" с ") || str.contains(" о ")){
-                System.out.println(true);
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }*/
+        Map<Character, Character> statistic = newMap(enc, white);
+        System.out.println(statistic);
 
+        statisticAnalize(statistic, path3);
     }
 
     public static void bruteForceDecoder(Path path){
@@ -103,6 +109,59 @@ public class GitLearning {
             System.out.println("File not found");
         }
         return charsMap;
+    }
+
+    public static Map<Character, Character> newMap(Map<Character, Integer> encryptedMap, Map<Character, Integer> testMap){
+        Map<Character, Character> newMap = new HashMap<>();
+        int len = Math.min(encryptedMap.size(), testMap.size());
+
+        Iterator<Character> iterator = testMap.keySet().iterator();
+        while (iterator.hasNext() && len != 0){
+            char enc = maxChar(encryptedMap);
+            char test = maxChar(testMap);
+            newMap.put(enc, test);
+            encryptedMap.remove(enc);
+            testMap.remove(test);
+            len--;
+        }
+        return newMap;
+    }
+
+    public static char maxChar(Map<Character, Integer> countedMap){
+        int max = 0;
+        char maxChar = ' ';
+        for (char temp : countedMap.keySet()) {
+            if (countedMap.get(temp) > max) {
+                max = countedMap.get(temp);
+                maxChar = temp;
+            }
+        }
+        return maxChar;
+    }
+
+    public static void statisticAnalize (Map<Character, Character> charsMap, Path filePath){
+        Path path2 = Path.of("Texts\\encodeWithAnalize.txt");
+
+        try(FileReader fileReader = new FileReader(String.valueOf(filePath));
+            FileWriter fileWriter = new FileWriter(String.valueOf(path2))){
+            while (fileReader.ready()){
+                char temp = (char) fileReader.read();
+                String s = "" + temp;
+                if(s.equals("\n")){
+                    fileWriter.write("\n");
+                    continue;
+                }
+                if(charsMap.containsKey(temp)){
+                    fileWriter.write(charsMap.get(temp));
+                } else {
+                    fileWriter.write(temp);
+                }
+
+            }
+        } catch (IOException e){
+            System.out.println("Файл не найден");
+        }
+        System.out.println("Закодированный файл можно найти по пути - " + path2.toAbsolutePath());
     }
 
     public static void encodeFunction(Path filePath, int key){
